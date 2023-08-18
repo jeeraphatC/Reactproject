5ed3d59ae82c17f72c68b33e9487b829ef9c5e45
Norrapat0 <119600305+Norrapat0@users.noreply.github.com>
2023-08-18 21:37:42 +0700

    styly and algoritm

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Home from './features/Home';
-import './App.css';
+
+import Navbar from './features/Navbar';
 
 function App() {
   return (
     <>
+    <Navbar/>
       <Home/>       
     </>
   );
