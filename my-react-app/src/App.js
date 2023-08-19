8d20c752c39435ad3445bf88067b4d32aadbcb42
Tunk07 <jeeraphat_c@cmu.ac.th>
2023-08-19 14:42:59 +0700

    navbar

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Navbar from './features/Navbar';
 import Home from './features/Home';
 import './App.css';
 
 function App() {
   return (
     <>
+    <Navbar />
       <Home/>       
     </>
   );
---
+++ my-react-app/src/features/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/services">Services</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
