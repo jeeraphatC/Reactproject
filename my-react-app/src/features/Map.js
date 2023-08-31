c38870cc254bebc59bcf559aa33270bf4c0105fb
Tunk07 <jeeraphat_c@cmu.ac.th>
2023-08-31 19:22:44 +0700

    map2

--- my-react-app/src/features/Map.js
+++ my-react-app/src/features/Map.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import './Map.css';
 
 const Map = () => {
     const center = [13.7563, 100.5018]; // Thailand's coordinates
 
     return (
-        <MapContainer center={center} zoom={6} style={{ height: '500px', width: '100%' }}>
-            <TileLayer
+        <MapContainer center={center} zoom={6} className="MapContainer">
+            <TileLayer 
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
-            <Marker position={center}>
-                <Popup>Thailand</Popup>
-            </Marker>
         </MapContainer>
     );
 };
